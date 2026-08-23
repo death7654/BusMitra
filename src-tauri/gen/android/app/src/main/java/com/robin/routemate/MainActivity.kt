1ad7a23696aedf85289e8bbf3f5c61779c0e9f49
Robinson Arysseril <72635727+death7654@users.noreply.github.com>
2026-08-23 14:36:49 +0530

    initial_commit

---
+++ src-tauri/gen/android/app/src/main/java/com/robin/routemate/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.robin.routemate
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
